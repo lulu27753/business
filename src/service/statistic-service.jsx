43d937eba3de5de279997e7121932cf298617a8f
lulu <lulu27753@163.com>
2018-06-17 18:44:42 +0800

    +home

---
+++ src/service/statistic-service.jsx
@@ -0,0 +1,17 @@
+// 统计数据
+
+import MUtil from 'util/mm.jsx'
+
+const _mm = new MUtil();
+
+class Statistic {
+    // 首页数据统计
+    getHomeCount() {
+        return _mm.request({
+            url: '/manage/statistic/base_count.do'
+        });
+    }
+}
+
+export default Statistic;
+
