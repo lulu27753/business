87816a120ae779f2cc7fdb605fc913cec7fb2b1c
lulu <lulu27753@163.com>
2018-06-18 19:18:57 +0800

    商品管理_添加商品_级联分类选择器

--- src/service/product-service.jsx
+++ src/service/product-service.jsx
@@ -91,19 +91,19 @@ class Product {
     //         data: product
     //     });
     // }
-    // /*
-    // *  品类相关
-    // */
-    // // 根据父品类id获取品类列表
-    // getCategoryList(parentCategoryId) {
-    //     return _mm.request({
-    //         type: 'post',
-    //         url: '/manage/category/get_category.do',
-    //         data: {
-    //             categoryId: parentCategoryId || 0
-    //         }
-    //     });
-    // }
+    /*
+    *  品类相关
+    */
+    // 根据父品类id获取品类列表
+    getCategoryList(parentCategoryId) {
+        return _mm.request({
+            type: 'post',
+            url: '/manage/category/get_category.do',
+            data: {
+                categoryId: parentCategoryId || 0
+            }
+        });
+    }
     // // 新增品类
     // saveCategory(category) {
     //     return _mm.request({
